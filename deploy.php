3a2d9f4d877fcffb8a96519d288a5ce65f1bdb14
とても痛い <sirdennice@gmail.com>
2023-01-08 04:49:53 -0500

    removing outputs from deployment script

--- deploy.php
+++ deploy.php
@@ -11,27 +11,8 @@ $commands = array(
     'test -e /usr/share/update-notifier/notify-reboot-required && echo "system restart required"',
 );
 
-$output = "\n";
-
-$log = "####### ".date('Y-m-d H:i:s'). " #######\n";
-
 foreach($commands AS $command){
-    // Run it
     $tmp = shell_exec("$command 2>&1");
-    // Output
-    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-    $output .= htmlentities(trim($tmp)) . "\n";
-
-    $log  .= "\$ $command\n".trim($tmp)."\n";
 }
 
-$log .= "\n";
-
-file_put_contents ('deploy-log.txt',$log,FILE_APPEND);
-
-echo $output; 
-
 ?>
-</pre>
-</body>
-</html>
