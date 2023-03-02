7af05586946a4b6009c723d9b08f601051315678
asarepok <poasare@icloud.com>
2023-03-02 06:27:58 -0500

    updated intro text; added memoji avi

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  new TypeIt("#intro", {
-    speed: 100,
-    startDelay: 900,
-  })
-    .type("Peter", { delay: 100 })
-    .pause(80)
-    .type(" Adare")
-    .pause(120)
-    .move(-3)
-    .delete(1)
-    .type("s")
-    .move(3, { instant: false })
-    .type(" :(", { delay: 100 })
-    .pause(300)
-    .delete(1)
-    .type(")")
-    .go();
-});
-
 function copy() {
   let npub = document.getElementById("npub");
   let copy_icon = document.getElementById("copy-icon");
